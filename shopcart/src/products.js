332d4aad20fc96588dff0527bfd955b202a3b659
Shandira <shandira.ferguson@gmail.com>
2023-01-14 10:09:22 -0800

    state data sucessfully added

---
+++ shopcart/src/products.js
@@ -0,0 +1,32 @@
+const Catalog = [
+  {
+    id: 1,
+    name: 'Unisex Cologne',
+    img: '/products/cologne.jpg',
+    alt: 'Unisex Cologne Image',
+    quantity: 0
+  },
+  {
+    id: 2,
+    name: 'Apple iWatch',
+    img: '/products/iwatch.jpg',
+    alt: 'Apple iWatch Image',
+    quantity: 0
+  },
+  {
+    id: 3,
+    name: 'Unique Mug',
+    img: '/products/mug.jpg',
+    alt: 'Unique Mug Image',
+    quantity: 0
+  },
+  {
+    id: 4,
+    name: 'Mens Wallet',
+    img: '/products/wallet.jpg',
+    alt: 'Mens Wallet Image',
+    quantity: 0
+  },
+]
+
+export default Catalog;
