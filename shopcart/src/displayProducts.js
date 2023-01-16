87f669eed1e1978aad75fdda283738b59bf5bc60
Shandira <shandira.ferguson@gmail.com>
2023-01-16 15:26:04 -0800

    states + modal successfully added

--- shopcart/src/displayProducts.js
+++ shopcart/src/displayProducts.js
@@ -1,27 +1,61 @@
 import React from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
+import { useState } from "react";
+import { Modal } from "react-bootstrap";
 
 function DisplayProducts( props ) {
+
+  const [ show, setShow ] = useState( false );  //to show modal
+  const [ showImg, setShowImg ] = useState( {} );
+
+  //handle calling from event clicks
+  const handleClose = () => setShow( false );
+  const handleShow = ( product ) => {
+    setShow( true );
+    setShowImg( product );
+  }
+
   return (
-    <ListGroup>
-      {props.productItems.map( ( item ) => (
-        <ListGroupItem
-          className='List-items'
-          key={item.id}
-        >
-          <div className='Item-name'>
-            {item.name}
-          </div>
-          <span className='Item-contents'>
-            <img src={item.img} alt="" className='Item-img' />
-            <div className='Quantity-contents'>
-              Quantity
-              <span className='Quantity-box'>{item.quantity}</span>
+    <div>
+      <ListGroup>
+        {props.productItems.map( ( product ) => (
+          <ListGroupItem
+            className='List-items'
+            key={product.id}
+          >
+            <div className='Item-name'>
+              {product.name}
             </div>
-          </span>
-        </ListGroupItem>
-      ) )}
-    </ListGroup>
+            <span className='Item-contents'>
+              <img
+                src={product.img}
+                alt=""
+                className='Item-img'
+                onClick={() => handleShow( product )} />
+              <div className='Quantity-contents'>
+                Quantity
+                <span className='Quantity-box'>{product.quantity}</span>
+              </div>
+            </span>
+          </ListGroupItem>
+        ) )}
+      </ListGroup>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>{showImg.name}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <img
+            src={showImg.img}
+            width="350"
+            alt={showImg.alt}
+            className="mx-5"
+          />
+          <p><span className="text-dark">Ratings: </span>{showImg.ratings}/5</p>
+        </Modal.Body>
+      </Modal>
+    </div>
   )
 }
 
