f585fcf9ede720e0003d79e0b90371c31f1b4a4d
Shandira <shandira.ferguson@gmail.com>
2023-01-17 14:50:44 -0800

    DisplayProducts successfully passed via route

--- shopcart/src/navbar.js
+++ shopcart/src/navbar.js
@@ -1,28 +1,53 @@
+import "./App.css";
 import { faShoppingCart, faR } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { Link, BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import DisplayProducts from './displayProducts';
 
-function Nav() {
+function Nav( props ) {
   return (
-    <nav className='Shop-nav'>
-      <ul>
-        <li>
-          <h1>Shop 2
-            <FontAwesomeIcon
-              icon={faR}
-              className="fa-sm bg-white rounded-circle text-info py-2 px-3">
-            </FontAwesomeIcon>
-            eact
-          </h1>
-        </li>
-        <li>
-          <span className='Cart-section'>
-            <FontAwesomeIcon icon={faShoppingCart} className='mx-3' />
-            0 items
-          </span>
-        </li>
-      </ul>
-    </nav>
+    <Router>
+      <nav className='Shop-nav'>
+        <ul>
+          <li>
+            <Link to="/" className="Nav-links">
+              <h1>Shop 2
+                <FontAwesomeIcon
+                  icon={faR}
+                  className="fa-sm bg-white rounded-circle text-info py-2 px-3">
+                </FontAwesomeIcon>
+                eact
+              </h1>
+            </Link>
+          </li>
+          <li>
+            <Link to="/cart" className="Nav-links">
+              <span className='Cart-section'>
+                <FontAwesomeIcon
+                  icon={faShoppingCart}
+                  className='mx-3' />
+                0 items
+              </span>
+            </Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        {/* <Route path="/cart" element={<Cart />} /> */}
+        <Route
+          path="/"
+          element={
+            <DisplayProducts
+              productItems={props.productItems}
+              addItem={props.addItem}
+              removeItem={props.removeItem}
+            />
+          }
+        />
+      </Routes>
+    </Router>
   )
 }
 
