7f9f1dfc2bd7838e4220ef7ead568ed9cdea7bbc
Shandira <shandira.ferguson@gmail.com>
2023-01-17 14:48:37 -0800

    Data successfully passed through Nav component alone

--- shopcart/src/App.js
+++ shopcart/src/App.js
@@ -3,7 +3,6 @@ import React, { Component } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { ProductsData } from './products';
 import Nav from './navbar'
-import DisplayProducts from './displayProducts';
 
 class App extends Component {
   constructor( props ) {
@@ -28,10 +27,11 @@ class App extends Component {
   render() {
     return (
       <div>
-        <Nav />
-        <main>
-          <DisplayProducts productItems={this.state.ProductsList} addItem={this.handleAddedItem} removeItem={this.handleRemovedItem}></DisplayProducts>
-        </main>
+        <Nav
+          productItems={this.state.ProductsList}
+          addItem={this.handleAddedItem}
+          removeItem={this.handleRemovedItem}
+        />
       </div>
     );
   }
