061d64a73c3da5c27ed44a4f26aeb84fe927ecec
Shandira <shandira.ferguson@gmail.com>
2023-01-22 11:30:05 -0800

    checkout button added to cart page

--- shopcart/src/cart.js
+++ shopcart/src/cart.js
@@ -30,6 +30,13 @@ function Cart( props ) {
           </ListGroupItem>
         ) )}
       </ListGroup>
+      <button
+        type="button"
+        className="btn btn-primary mt-4"
+      // onClick={ }
+      >
+        Check Out
+      </button>
     </div>
 
   )
