c7ce2f4e62e2a1b03a1f628175776c43b72b5d3b
Shandira <shandira.ferguson@gmail.com>
2023-01-22 14:24:23 -0800

    Cart items logic successfully completed

--- shopcart/src/App.js
+++ shopcart/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { Component } from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+// import "bootstrap/dist/css/bootstrap.min.css";
 import { ProductsData } from './products';
 import Nav from './navbar'
 
--- shopcart/src/cart.js
+++ shopcart/src/cart.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { ListGroup, ListGroupItem } from "reactstrap";
 import "./App.css";
+import { Link } from "react-router-dom";
+// import FbLogin from "./fbLogin";
 
 function Cart( props ) {
+  // console.log( 'PROPS in cart: ', props.cartItems, props.cartItems.length );
   return (
     <div className="Cart-items p-5 mx-5 my-2">
       <h2>Your Cart Items</h2>
@@ -30,15 +33,28 @@ function Cart( props ) {
           </ListGroupItem>
         ) )}
       </ListGroup>
-      <button
-        type="button"
-        className="btn btn-primary mt-4"
-      // onClick={ }
-      >
-        Check Out
-      </button>
-    </div>
 
+      {
+        ( !props.cartItems.length )
+          ? <React.Fragment>
+            <p>There are {props.cartItems.length} items in your cart.</p>
+            <Link
+              to="/"
+              className="btn btn-success mt-2"
+            // onClick={ }
+            >
+              Continue Shop
+            </Link>
+          </React.Fragment>
+          : <button
+            type="button"
+            className="btn btn-primary mt-4"
+          // onClick={ }
+          >
+            Check Out
+          </button>
+      }
+    </div>
   )
 }
 
